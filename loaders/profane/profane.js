47e709fa5919e6d6659ef2f73dd4b14c3cdcf8eb
lizzy596 <colleenboehme@colleens-Mini.localdomain>
2022-09-02 12:38:19 -0500

    added profane words list loader script and model

---
+++ loaders/profane/profane.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const config = require('../../src/config/config');
+const wordsArray = require('./profane.json');
+const { Profane } = require('../../src/models');
+/* const Profane = require('../../src/models/profane.model.js'); */
+const logger = require('../../src/config/logger');
+
+mongoose.connect(config.mongoose.url, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+const profaneObjects = wordsArray.map((word) => ({ word }));
+
+Profane.insertMany(profaneObjects)
+  .then(function () {
+    logger.info('Data inserted');
+    process.exit(0);
+  })
+  .catch(function (error) {
+    logger.error(error.message);
+    process.exit(1);
+  });
