b82569c7594e8d83d18fbfd85159f16d374bc46a
Noah Reed <Nore5515@gmail.com>
2023-04-25 12:02:32 -0500

    udpated boilerplate with new dependencies

--- src/docs/postman.js
+++ src/docs/postman.js
@@ -1,7 +1,7 @@
 const Converter = require('openapi-to-postmanv2');
 
 module.exports = (openapiData) => (req, res, next) => {
-  Converter.convert({ type: 'string', data: openapiData }, {}, (err, conversionResult) => {
+  Converter.convert({ type: 'json', data: openapiData }, {}, (err, conversionResult) => {
     if (!conversionResult.result) {
       next(conversionResult.reason);
     } else {
